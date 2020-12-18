c764efa21f689de55807dca09bea4f51c1c6d4d8
Ewa Kadziolka <kalistaewa@gmail.com>
2020-12-18 13:16:54 +0100

    contact form corrected

--- js/form.js
+++ js/form.js
@@ -1,10 +1,10 @@
 /* elements selection */
 
 var form = $('form');
-var prenomInput = $('prenom');
-var nomInput = $('nom');
-var emailInput = $('email');
-var messageInput = $('message');
+var prenomInput = $('.prenom');
+var nomInput = $('.nom');
+var emailInput = $('.email');
+var messageInput = $('.message');
 var div = $('div.control');
 
 /* function soumission pour recouper touts les champs */
@@ -20,6 +20,7 @@ function onSubmit(event) {
     var nom = nomInput.val();
     var email = emailInput.val();
     var message = messageInput.val();
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     console.log(prenom);
     console.log(nom);
@@ -28,7 +29,7 @@ function onSubmit(event) {
 
     if (prenom.length == 0) {
         // injecter le texte "le champ doit contenir un prenom" dans le p qui suit l'input
-        prenomInput.next().text("le champ doit contenir un prenom").show();
+        prenomInput.next().text("le champ doit contenir votre prenom").show();
         nbErrors++;
     } else if (prenom.length < 2) {
         prenomInput.next().text("le prénom doit comporter au moins 2 caractères").show();
@@ -36,36 +37,36 @@ function onSubmit(event) {
     }
 
     if (nom.length == 0) {
-        // injecter le texte "le champ doit contenir un prenom" dans le p qui suit l'input
-        prenomInput.next().text("le champ doit contenir un prenom").show();
+        // injecter le texte "le champ doit contenir votre prenom" dans le p qui suit l'input
+        nomInput.next().text("le champ doit contenir votre nom").show();
         nbErrors++;
-    } else if (prenom.length < 2) {
-        prenomInput.next().text("le nom doit comporter au moins 2 caractères").show();
-        nbErrors++;
-    }
-
-    if (email.length == 0) {
-        // injecter le texte "le champ doit contenir un prenom" dans le p qui suit l'input
-        prenomInput.next().text("le champ doit contenir un prenom").show();
-        nbErrors++;
-    } else if (prenom.length < 5) {
-        prenomInput.next().text("l'email doit comporter au moins 5 caractères").show();
+    } else if (nom.length < 2) {
+        nomInput.next().text("le nom doit comporter au moins 2 caractères").show();
         nbErrors++;
     }
 
-    if (prenom.length == 0) {
-        // injecter le texte "le champ doit contenir un prenom" dans le p qui suit l'input
-        prenomInput.next().text("le champ doit contenir un prenom").show();
+    if (email.length == 0 && email.match(regex)) {
+        // injecter le texte "le champ doit contenir votre nom" dans le p qui suit l'input
+        emailInput.next().text("le champ doit contenir votre email").show();
+        nbErrors++;
+    } else if (email.length < 5) {
+        emailInput.next().text("l'email doit comporter au moins 5 caractères").show();
+        nbErrors++;
+    }
+
+    if (message.length == 0) {
+        // injecter le texte "le champ doit contenir un message de plus de 10 caractéres" dans le p qui suit l'input
+        messageInput.next().text("le champ doit contenir votre message").show();
         nbErrors++;
     } else if (message.length < 10) {
-        prenomInput.next().text("le message doit comporter au moins 10 caractères").show();
+        messageInput.next().text("le message doit comporter au moins 10 caractères").show();
         nbErrors++;
     }
 
     if (nbErrors == 0) {
         div.removeClass('danger');
         div.addClass('success');
-        div.text("Coool, le formulaire a été envoyé !").slideDown();
+        div.text("Bravo, le formulaire a été envoyé !").slideDown();
         // poster le formulaire au serveur
     } else {
         div.removeClass('success');
